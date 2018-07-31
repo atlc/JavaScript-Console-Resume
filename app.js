782119e6a30d42cfcc27a7eb3616be27d7dadb82
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2018-07-31 12:11:02 -0500

    Took out other looping functions to practice multiple function calls per requirements. Implemented a displayInterests function to exhibit DRY principles.

--- app.js
+++ app.js
@@ -1,41 +1,48 @@
 let myName = "Andrew Cartwright";
 let careerGoal = "Full-Stack Software Engineer";
 let selfDescription = "Birmingham, AL native. Recent graduate and current employee of Jefferson State, and current employee of a vet clinic.";
-let myInterests = ["Programming", "Electronics", "Outdoors", "Animals", "Stranger Things"];
-let myPositions = [
-    {company: "Jefferson State Community College", jobTitle: "Computer Lab Technician", description: "Maintained the 4 computer labs on the top floor, including supporting users with all desktop needs, providing general Windows support and miscellaneous hardware & printer support."},
-    {company: "Caldwell Mill Animal Clinic", jobTitle: "Kennel Technician", description: "Responsibilities included day-to-day upkeep of the clinic and care for the animals present for boarding, daycare, and veterinary care, and assisting the doctors and the vet techs in their roles as needed."},
-    {company: "Uber & Lyft", jobTitle: "Rideshare Driver", description: "While operating as a rideshare driver for both services, I provided excellent service, maintaining a perfect 5.00/5.00 rating and consistently received the \"Above and Beyond\" driver award with Uber."},
-    {company: "Vincari", jobTitle: "Software Engineer", description: "Created new features and fixed bugs for our EMR webapp, on a stack of Ruby on Rails, Angular.js, MySQL, and AWS (EC2, VPC, and S3). A notable project I made was a logging framework to provide our Implementation & Support team an invaluable tool with which they could easily assist our clients."},
-    {company: "Hubbard Systems", jobTitle: "Application Support Analyst", description: "I trained and assisted our clients in everyday usage of our accounting application, and in addition to gaining valuable accounting, legal, and computer support skills, got involved with both Windows & Linux server administration, and T-SQL + SSMS with for a subset of our application."},
-    {company: "Greystone Golf and Country Club", jobTitle: "Server, Bartender, Event Staff", description: "Worked in various roles related to dining - including serving in a fast, casual, everyday environment all the way to our fine dining. As event staff, I worked closely with clients as we prepared and setup events from small birthday parties to holiday parties, to giant golf tournaments or weddings spanning hundreds of people."}
-];
+let myInterests = ["Programming", "Electronics", "Outdoors", "Animals", "Stranger Things", "Auto mechanic work"];
 
-
-function displayPosition(company, jobTitle, description) {
-    console.log("\nExperience:");
-    for (i in myPositions) {
-        console.log("* " + myPositions[i].company + ", " + myPositions[i].jobTitle + "\n\t- " + myPositions[i].description + "\n\n");
-    }
+function displayPosition(jobTitle, company, description) {
+    console.log("* " + jobTitle + " at " + company + "\n\t- " + description);
 }
 
 function displaySkill(skill, isCool) {
-
+    if (isCool) {
+        console.log("* BAM: " + skill);
+    } else {
+        console.log("* " + skill);
+    }
 }
 
 function displayInterests() {
     console.log("\nMy interests:");
     for (i of myInterests) {
-        console.log("\t-" + i);
+        console.log("* " + i);
     }
 }
 
 function displayALL() {
-    console.log("Name:\t\t" + myName.toUpperCase());
+    console.log("Name:\t\t\t" + myName.toUpperCase());
     console.log("Career goal:\t" + careerGoal);
     console.log("Description:\t" + selfDescription);
     displayInterests();
-    displayPosition();
+    console.log("\nExperience:");
+    displayPosition("Computer Lab Technician", "Jefferson State Community College", "Maintained the 4 computer labs on the top floor, including supporting users with all desktop needs, providing general Windows support and miscellaneous hardware & printer support.");
+    displayPosition("Kennel Technician", "Caldwell Mill Animal Clinic", "Responsibilities included day-to-day upkeep of the clinic and care for the animals present for boarding, daycare, and veterinary care, and assisting the doctors and the vet techs in their roles as needed.");
+    displayPosition("Rideshare Driver", "Uber & Lyft", "Provided excellent service, maintaining a perfect 5.00/5.00 rating and consistently received the \"Above and Beyond\" driver award with Uber.");
+    displayPosition("Software Engineer", "Vincari", "Created new features and fixed bugs for our EMR webapp, on a stack of Ruby on Rails, Angular.js, MySQL, and AWS (EC2, VPC, and S3). A notable project I made was a logging framework to provide our Implementation & Support team an invaluable tool with which they could easily assist our clients.");
+    displayPosition("Application Support Analyst", "Hubbard Systems", "I trained and assisted our clients in everyday usage of our accounting application, and in addition to gaining valuable accounting, legal, and computer support skills, got involved with both Windows & Linux server administration, and T-SQL + SSMS with for a subset of our application.");
+    displayPosition("Server, Bartender, Event Staff", "Greystone Golf and Country Club", "Worked in various roles related to dining - including serving in a fast, casual, everyday environment all the way to our fine dining. As event staff, I worked closely with clients as we prepared and setup events from small birthday parties to holiday parties, to giant golf tournaments or weddings spanning hundreds of people.");
+    console.log("\nSkills:");
+    displaySkill("Cat wrangling", true);
+    displaySkill("Spanish", false);
+    displaySkill("Electric bass", true);
+    displaySkill("Cooking", false);
+    displaySkill("HTML", false);
+    displaySkill("Vanilla CSS", false);
+    displaySkill("CSS with Bootstrap", true);
+    displaySkill("Vanilla JS", true);
 }
 
 displayALL();
